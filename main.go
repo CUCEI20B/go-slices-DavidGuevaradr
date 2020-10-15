e0b89e8ffdf71b0e248345d61deca4d777df3a17
David Guevara <david.guevara4137@alumnos.udg.mx>
2020-10-14 21:21:42 -0500

    Slices.

--- main.go
+++ main.go
@@ -4,4 +4,32 @@ import "fmt"
 
 func main()  {
 	fmt.Println("Hola Mundo!") // eliminar esta línea
+}import "fmt"
+
+func main() {
+	var tam int
+	var num int
+	var aux int
+	
+	//fmt.Println("Tamaño del slice: ")
+	fmt.Scan(&tam)
+
+	s := make([]int, 0, tam)
+
+	for i := 0 ; i < tam ; i++ {
+		if i < tam {
+			//fmt.Println("ingrese un numero: ")
+			fmt.Scan(&num)
+			s = append(s,num);
+			aux += num
+		}		
+	}
+	fmt.Println(aux)
+	fmt.Println(len(s), cap(s))
+
+	for i, v := range s {
+		fmt.Println(i,v)
+	}
+
+	
 }
